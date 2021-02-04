af33fd9b6b890304484dc37ba1eb725082ee7fa1
Oscar Patricio <ext.oscar.patricio@farfetch.com>
2021-02-04 21:36:47 +0000

    fix: fixed example

--- examples/main.tf
+++ examples/main.tf
@@ -13,42 +13,16 @@ resource "azurerm_resource_group" "test" {
   location = var.location
 }
 
-resource "azurerm_network_security_group" "nsg1" {
-  name                = "test-${random_id.rg_name.hex}-nsg"
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-}
-
-resource "azurerm_route_table" "rt1" {
-  location            = azurerm_resource_group.test.location
-  name                = "test-${random_id.rg_name.hex}-rt"
-  resource_group_name = azurerm_resource_group.test.name
-}
-
 module "vnet" {
   source              = "../"
-  vnet_name           = "test-${random_id.rg_name.hex}-vnet"
+  name                = "test-${random_id.rg_name.hex}-vnet"
   resource_group_name = azurerm_resource_group.test.name
-  address_space       = ["10.0.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
-  nsg_ids = {
-    subnet1 = azurerm_network_security_group.nsg1.id
+  address_space       = ["10.0.0.0/22"]
+  tags = {
+    "example_tag" = "example_tag_value"
   }
-
-  subnet_service_endpoints = {
-    subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
-    subnet3 = ["Microsoft.AzureActiveDirectory"]
-  }
-
-  route_tables_ids = {
-    subnet1 = azurerm_route_table.rt1.id
-  }
-
   custom_tags = {
-    environment = "dev"
-    costcenter  = "it"
+    "example_custom_tag" = "example_custom_tag_value"
   }
 
   depends_on = [azurerm_resource_group.test]
